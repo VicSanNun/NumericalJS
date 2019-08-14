adf535780ca9f70ccec9760be9efb53afe5bc714
Victor Nunes <victornunes470@gmail.com>
2019-08-14 19:37:22 -0300

    Add methods for calculate equations roots

---
+++ src/classes/FindRoot.js
@@ -0,0 +1,19 @@
+class FindRoot {
+	
+	static Bhaskara(a, b, c) {
+		const delta = ((b*b)-(4*a*c));
+		
+		if(delta < 0) return ("Don't exists real roots");
+
+	    const x1 = (-b + Math.sqrt(delta))/2*a;
+	    const x2 = (-b - Math.sqrt(delta))/2*a;
+
+	    return [x1, x2];
+	}
+
+	static IncrementalSearch(fn, xmin, xmax, steps = 50.0) {
+		
+	}
+}
+
+module.exports = FindRoot
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const Integrate = require('./classes/Integrate');
 const Derivative = require('./classes/Derivative');
+const FindRoot = require('./classes/FindRoot');
 
 const fn = function(x) {
     return 2*(x*x);
@@ -9,6 +10,9 @@ const a = Integrate.Trapezio(fn, 1, 5);
 const b = Integrate.Simpson(fn, 1, 5);
 const c = Derivative.Diff(fn, 2, 0.000000001);
 
+const result = FindRoot.Bhaskara(1, -10, 24);
+
 console.log(a);
 console.log(b);
 console.log(c);
+console.log(result);
